4ce7c7da8d2474c08c498f7e6a1c95ac691b0757
dequan300 <48841256+dequan300@users.noreply.github.com>
2019-12-31 23:34:53 -0500

    update
    Made code better

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,26 +1,29 @@
 import React from "react";
-
-function App(props) {
-
+import Card from "./card";
+import Contacts from "../contacts";
+function App() {
   return (
-
     <div>
-     
-      <div className="card">
-        <div className="top">
-          <h2 className="name">{props.name}</h2>
-          <img className="circle-img"
-          src={props.img}
-            alt="avatar_img"
-          />
-        </div>
-        <div className="bottom">
-          <p>{props.tele}</p>
-          <p>{props.email}</p>
-        </div>
-      </div>
+      <h1 className="heading">My Contacts</h1>
+      <Card
+        name={Contacts[0].name}
+        email={Contacts[0].email}
+        tele={Contacts[0].phone}
+        img={Contacts[0].imgURL}
+      />
+      <Card
+        name={Contacts[1].name}
+        email={Contacts[1].email}
+        tele={Contacts[1].phone}
+        img={Contacts[1].imgURL}
+      />
+      <Card
+        name={Contacts[2].name}
+        email={Contacts[2].email}
+        tele={Contacts[2].phone}
+        img={Contacts[2].imgURL}
+      />
     </div>
-
   );
 }
 
